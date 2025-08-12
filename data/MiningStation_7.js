680a03c033b641a4bd82b90c84266047f4c0ecf6
camkool <campbellwardnz@gmail.com>
2025-08-12 21:44:36 +1200

    Exploration update

---
+++ data/MiningStation_7.js
@@ -0,0 +1,1 @@
+var json_MiningStation_7 = {"type":"FeatureCollection","name":"MiningStation_7","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1","Name":"Sorel Mining Station","Owner":"Atlas Mining Co."},"geometry":{"type":"Point","coordinates":[0.020492574796413,-0.008493698124148]}}]}
