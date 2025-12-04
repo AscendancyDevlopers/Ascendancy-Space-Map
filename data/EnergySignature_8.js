ee806c947f007a2debb65943237db91f4eb5c917
camkool <campbellwardnz@gmail.com>
2025-12-04 22:38:21 +1300

    Updated

---
+++ data/EnergySignature_8.js
@@ -0,0 +1,1 @@
+var json_EnergySignature_8 = {"type":"FeatureCollection","name":"EnergySignature_8","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1","Name":"Unknown  Energy Signature"},"geometry":{"type":"Point","coordinates":[0.024597595202251,-0.01747442614349]}}]}
